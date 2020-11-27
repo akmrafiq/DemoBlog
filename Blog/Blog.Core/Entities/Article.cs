e4e1631ee96f468b8e26671ce6396ceec32e07f9
AKM Rafiqul Alam <akmrafiq450@gmail.com>
2020-11-28 00:17:47 +0600

    Add Project Structure

---
+++ Blog/Blog.Core/Entities/Article.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blog.Core.Entities
+{
+    public class Article
+    {
+        public virtual int Id { get; set; }
+        public virtual string Title { get; set; }
+        public virtual string Description { get; set; }
+        public virtual string Author { get; set; }
+        public virtual string ImageUrl { get; set; }
+        public virtual  DateTime CreateDate { get; set; }
+    }
+}
---
+++ Blog/Blog.Core/Entities/ArticleCategory.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blog.Core.Entities
+{
+    public class ArticleCategory
+    {
+        public virtual int ArticleId { get; set; }
+        public virtual int CategoryId { get; set; }
+    }
+}
