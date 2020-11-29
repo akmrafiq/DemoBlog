21666bcb9a313a27b6451b5dc6ed6ce15ed52cda
AKM Rafiqul Alam <akmrafiq450@gmail.com>
2020-11-29 21:41:23 +0600

    Theme Integration Done

--- Blog/Blog.Web/Areas/Admin/AdminAreaRegistration.cs
+++ Blog/Blog.Web/Areas/Admin/AdminAreaRegistration.cs
@@ -18,7 +18,7 @@ namespace Blog.Web.Areas.Admin
             context.MapRoute(
                 "Admin_default",
                 "Admin/{controller}/{action}/{id}",
-                new { controller = "Article", action = "Index", id = UrlParameter.Optional }
+                new { controller = "DashBoard", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
